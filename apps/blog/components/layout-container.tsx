d7f5bef37fedf9b134300003c6cdb27fdf7f5e47
Juan Morais <timrodz@icloud.com>
2026-02-14 13:39:29 +1300

    refactor: Blog layout responsiveness

--- apps/blog/components/layout-container.tsx
+++ apps/blog/components/layout-container.tsx
@@ -2,6 +2,8 @@ import { PropsWithChildren } from "react";
 
 export function LayoutContainer({ children }: PropsWithChildren) {
   return (
-    <div className="max-w-5xl mx-auto px-6 py-16 space-y-10">{children}</div>
+    <div className="max-w-full md:max-w-2xl lg:max-w-4xl mx-auto px-6 py-16 space-y-10">
+      {children}
+    </div>
   );
 }
--- apps/blog/components/page-container.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export function PageContainer({ children }: PropsWithChildren) {
-  return (
-    <main className="max-w-5xl mx-auto px-6 py-16 space-y-10">{children}</main>
-  );
-}
